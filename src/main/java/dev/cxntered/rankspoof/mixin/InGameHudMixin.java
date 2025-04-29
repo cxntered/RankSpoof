2ca7c2bbf2dda4348c18205f4d2c0d3180e87b32
cxntered <cxntered@duck.com>
2025-04-29 16:58:49 +1000

    ✨ feature: scoreboard rank spoof

---
+++ src/main/java/dev/cxntered/rankspoof/mixin/InGameHudMixin.java
@@ -0,0 +1,27 @@
+package dev.cxntered.rankspoof.mixin;
+
+import dev.cxntered.rankspoof.config.ModConfig;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+    @ModifyVariable(
+            method = "method_55439", // renderScoreboardSidebar
+            at = @At(value = "STORE"),
+            index = 6
+    )
+    private Text spoofScoreboardRank(Text text) {
+        if (ModConfig.CONFIG.instance().enabled && text.getString().matches("Rank: .+")) {
+            String rank = ModConfig.CONFIG.instance().spoofedRank
+                    .replace('&', '§')
+                    .replace("[", "")
+                    .replace("]", "");
+            return Text.of("Rank: " + rank);
+        }
+        return text;
+    }
+}
