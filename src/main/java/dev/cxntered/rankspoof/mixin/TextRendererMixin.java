b3f668744bef881fa2b0e8a427090bce4684483a
cxntered <cxntered@duck.com>
2025-04-29 18:02:48 +1000

    🐛 fix: properly spoof text width
    while i was trying to fix nametag centering, i realised i could kill two birds with one stone by directly spoofing `getWidth`, also removing the need for the tablist fix.

--- src/main/java/dev/cxntered/rankspoof/mixin/TextRendererMixin.java
+++ src/main/java/dev/cxntered/rankspoof/mixin/TextRendererMixin.java
@@ -4,12 +4,20 @@ import dev.cxntered.rankspoof.config.ModConfig;
 import dev.cxntered.rankspoof.text.SpoofedOrderedText;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.OrderedText;
+import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TextRenderer.class)
 public abstract class TextRendererMixin {
+    @Shadow
+    public abstract int getWidth(OrderedText text);
+
     @ModifyVariable(
             method = "drawLayer(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)F",
             at = @At(value = "HEAD"),
@@ -25,8 +33,18 @@ public abstract class TextRendererMixin {
             at = @At(value = "HEAD"),
             argsOnly = true
     )
-    private OrderedText spoofGetWidth(OrderedText orderedText) {
+    private OrderedText spoofGetWidthOrderedText(OrderedText orderedText) {
         if (ModConfig.CONFIG.instance().enabled) return new SpoofedOrderedText(orderedText);
         return orderedText;
+    }
+
+    @Inject(
+            method = "getWidth(Lnet/minecraft/text/StringVisitable;)I",
+            at = @At(value = "HEAD"),
+            cancellable = true
+    )
+    private void spoofGetWidthStringVisitable(StringVisitable stringVisitable, CallbackInfoReturnable<Integer> cir) {
+        if (ModConfig.CONFIG.instance().enabled && stringVisitable instanceof Text text)
+            cir.setReturnValue(this.getWidth(new SpoofedOrderedText(text.asOrderedText())));
     }
 }
